5c2091de64535f8921002915a715010cdf94a32c
santohenrique <fernandeshenrique01@gmail.com>
2020-05-06 20:52:17 -0300

    Adicionando comentários aos métodos e testando criação de pull requests pelo GitHub

--- PaoNaChapa.Heranca/PagamentoCartaoDebito.cs
+++ PaoNaChapa.Heranca/PagamentoCartaoDebito.cs
@@ -9,11 +9,20 @@ namespace PaoNaChapa.Heranca
     /// </summary>
     public class PagamentoCartaoDebito : PagamentoCartao
     {
+        /// <summary>
+        /// Validar as credencias de forma específica para cartão de débito
+        /// </summary>
+        /// <param name="senha">Senha</param>
+        /// <returns>True caso obtenha sucesso na validação</returns>
         protected override bool ValidarCredenciais(string senha)
         {
             return true;
         }
 
+        /// <summary>
+        /// Finalizar o pagamento, específico para cartão de débito
+        /// </summary>
+        /// <returns>True em caso de sucesso</returns>
         protected override bool CompletarPagamento()
         {
             return true;
